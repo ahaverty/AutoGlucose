efde0a999c3b137201e4864239adb5157fb357af
Alan Haverty <alan@ahaverty.com>
2015-07-23 21:59:42 +0100

    Added Measurement class for holding readings taken from the accu chek mobile.

---
+++ src/com/ahaverty/autoglucose/data/Measurement.java
@@ -0,0 +1,99 @@
+/**
+ * 
+ */
+package com.ahaverty.autoglucose.data;
+
+import org.joda.time.DateTime;
+
+/**
+ * @author Alan Haverty
+ *
+ */
+public class Measurement {
+	
+	final private int unitDifference = 18;
+
+	public enum ReadingCategory {
+		BEFORE_MEAL, AFTER_MEAL, OTHER, CONTROL_TEST;
+	};
+
+	public enum Unit {
+		MMOL("mmol/l"), MGDL("mg/dl");
+		private final String value;
+
+		private Unit(String s) {
+			value = s;
+		}
+
+		public boolean equalsName(String otherName) {
+			return (otherName == null) ? false : value.equals(otherName);
+		}
+
+		public String toString() {
+			return this.value;
+		}
+	};
+
+	// Date;Time;Result;Unit;Temperature warning;Out of target
+	// range;Other;Before meal;After meal;Control test
+	private DateTime dateTime;
+	private double readingMgdl;
+	private Unit unit;
+	private boolean temperatureWarning;
+	private boolean outOfTargetRange;
+	private boolean other;
+	private ReadingCategory readingCategory;
+	
+	
+	public DateTime getDateTime() {
+		return dateTime;
+	}
+	public void setDateTime(DateTime dateTime) {
+		this.dateTime = dateTime;
+	}
+	public Unit getUnit() {
+		return unit;
+	}
+	public void setUnit(Unit unit) {
+		this.unit = unit;
+	}
+	public boolean isTemperatureWarning() {
+		return temperatureWarning;
+	}
+	public void setTemperatureWarning(boolean temperatureWarning) {
+		this.temperatureWarning = temperatureWarning;
+	}
+	public boolean isOutOfTargetRange() {
+		return outOfTargetRange;
+	}
+	public void setOutOfTargetRange(boolean outOfTargetRange) {
+		this.outOfTargetRange = outOfTargetRange;
+	}
+	public boolean isOther() {
+		return other;
+	}
+	public void setOther(boolean other) {
+		this.other = other;
+	}
+	public ReadingCategory getReadingCategory() {
+		return readingCategory;
+	}
+	public void setReadingCategory(ReadingCategory readingCategory) {
+		this.readingCategory = readingCategory;
+	}
+	public double getReadingMgdl() {
+		return readingMgdl;
+	}
+	public void setReadingMgdl(double readingMgdl) {
+		this.readingMgdl = readingMgdl;
+	}
+	public double getReadingMmol() {
+		return readingMgdl/unitDifference;
+	}
+	public void setReadingMmol(double readingMmol) {
+		this.readingMgdl = readingMmol*unitDifference;
+	}
+	
+	
+
+}
